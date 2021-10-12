f6b884bf4a80f11a33e26257bbd660380405b984
Matheus Martins <matheus.math27@hotmail.com>
2021-10-12 01:54:29 -0300

    fixing vercel deployment

--- expense-bff/src/main.js
+++ expense-bff/src/main.js
@@ -1,12 +1,22 @@
 import express, { json } from "express";
 import cors from "cors";
 import { config } from "dotenv";
-
-import routes from "./routes/index.js";
+import { Router } from "express";
+import {
+  createExpense,
+  findExpenseById,
+  findAllExpense,
+} from "../controllers/index.js";
 
 config();
 
 const app = express();
+const routes = Router();
+
+routes.get("/", (_req, response) => response.json({ version: "1.0.0" }));
+routes.post("/expense", createExpense);
+routes.get("/expense", findAllExpense);
+routes.get("/expense/:id", findExpenseById);
 
 app.use(json());
 app.use(
