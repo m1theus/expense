2ce2c1894ba7f0cb216d4bfafd104cf07667dc01
Matheus Martins <matheus.math27@hotmail.com>
2021-10-12 01:44:46 -0300

    test fixing cors problem

--- expense-bff/index.js
+++ expense-bff/index.js
@@ -15,7 +15,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin", "Accept"],
     origin: "*",
     credentials: true,
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
   })
 );
 app.use(routes);
