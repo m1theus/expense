0bfa038b357c7e80c6bac54784d25600b9bd64a7
Matheus Martins <matheus.math27@hotmail.com>
2021-10-12 01:13:37 -0300

    force axios accept http

--- expense-ui/src/Api.js
+++ expense-ui/src/Api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import https from "https";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_API ?? "http://localhost:1337",
@@ -7,6 +8,9 @@ const api = axios.create({
     "Content-Type": "application/json",
     accept: "application/json",
   },
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
 export default api;
